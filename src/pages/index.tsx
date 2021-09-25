d5d185da84d0e299a11de5fcf9e284cb8bf86215
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-09-26 02:10:27 +0900

    add home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,12 @@ export default function Index() {
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
         <h1>Web Log from Notion</h1>
+        <div style={{ textAlign: 'center' }}>
+          <p>たまにバグります…</p>
+          <a href="https://blog.izumiz.me/blog/renewal-blog">
+            NotionとNext.jsで作成してます．
+          </a>
+        </div>
       </div>
     </>
   )
