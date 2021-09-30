35cd29084cf99ca3371c0ff05c50e20837ad515f
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-09-30 20:13:35 +0900

    add contribution graph at blog home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,34 @@
+import dynamic from 'next/dynamic'
 import Header from '../components/header'
 import sharedStyles from '../styles/shared.module.css'
 
+const ReactGitHubCalendar = dynamic(() => import('react-ts-github-calendar'), {
+  ssr: false,
+})
 export default function Index() {
   return (
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <h1>Web Log from Notion</h1>
+        <h1>Blog Home</h1>
         <div style={{ textAlign: 'center' }}>
-          <p>たまにバグります…</p>
+          <p>
+            <s>たまにバグります…</s>
+            <br />
+            ISRをやめてSSGにしました.
+          </p>
           <a href="https://blog.izumiz.me/blog/renewal-blog">
-            Notion-blogとNext.jsで作成してます．
+            このブログはNotion-blogで作成しています
           </a>
+        </div>
+        <h2>進捗メーター</h2>
+        <div style={{ padding: '0 20%' }}>
+          <ReactGitHubCalendar
+            // tooltips
+            responsive
+            global_stats={false}
+            userName={'izumiz-dev'}
+          />
         </div>
       </div>
     </>
