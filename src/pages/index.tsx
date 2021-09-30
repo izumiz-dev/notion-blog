4725fb72a04df2da24ff67a52f9499c501b8caf3
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-09-30 19:50:04 +0900

    remove unuse file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import Header from '../components/header'
-// import ExtLink from '../components/ext-link'
-// import Features from '../components/features'
 import sharedStyles from '../styles/shared.module.css'
 
 export default function Index() {
@@ -12,7 +10,7 @@ export default function Index() {
         <div style={{ textAlign: 'center' }}>
           <p>たまにバグります…</p>
           <a href="https://blog.izumiz.me/blog/renewal-blog">
-            NotionとNext.jsで作成してます．
+            Notion-blogとNext.jsで作成してます．
           </a>
         </div>
       </div>
