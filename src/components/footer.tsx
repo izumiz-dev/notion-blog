3e1e50c37c6cd4133f667d6658fa28e027669d83
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-10-01 19:15:48 +0900

    add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,10 @@
-import ExtLink from './ext-link'
-
 export default function Footer() {
   return (
     <>
       <footer>
-        <span>Deploy your own!</span>
-        <ExtLink href="https://vercel.com/new/git/external?repository-url=https://github.com/ijjk/notion-blog/tree/main&project-name=notion-blog&repository-name=notion-blog">
-          <img
-            src="https://vercel.com/button"
-            height={46}
-            width={132}
-            alt="deploy to Vercel button"
-          />
-        </ExtLink>
-        <span>
-          or{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog">
-            view source
-          </ExtLink>
-        </span>
+        <div style={{ padding: '0 5vh', fontSize: '0.75rem' }}>
+          <a href="https://github.com/izumiz-dev">@izumiz-dev</a>
+        </div>
       </footer>
     </>
   )
