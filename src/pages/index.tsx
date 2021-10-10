f083ae49dcf6c3e61e73e33c22bd476666ba4a12
izumiz <42127166+izumiz-dev@users.noreply.github.com>
2021-10-10 23:33:15 +0900

    update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,26 @@ export default function Index() {
     <>
       <Header titlePre="Home" />
       <div className={sharedStyles.layout}>
-        <h1>Blog Home</h1>
-        <div style={{ textAlign: 'center' }}>
-          <p>🚧工事中 Under Construction🚧</p>
+        <h1>About</h1>
+        <div className={sharedStyles.page}>
+          <p>
+            主にJavaScript，TypeScript，
+            Reactなどを使いWebアプリなどを作成しています．
+            その中で役に立ったことなどをブログ記事として残しています．
+          </p>
+          <p>また，どうでもいいような日記も更新していくつもりです．</p>
+          <p>
+            本ブログは，
+            <a href="https://github.com/ijjk/notion-blog">Notion-blog</a>
+            をもとに作成し，Notionからの記事の取得は
+            <a href="https://developers.notion.com/">Notion API</a>
+            を利用しています．
+          </p>
         </div>
         <h2 style={{ color: '#000' }}>Contribution</h2>
+        <div style={{ textAlign: 'center' }}>
+          Github コントリビューションの状況です． いつでも監視いただけます．
+        </div>
         <div className={contactStyles.contribution}>
           <ReactGitHubCalendar
             responsive
